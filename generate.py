b5fef105d42d26203298d21e63afc8949a6d85a8
QualityDave <dave76ers@gmail.com>
2017-08-15 14:12:45 +0200

    file generated

--- generate.py
+++ generate.py
@@ -4,12 +4,26 @@ Created on 26 jul. 2017
 @author: Dave
 '''
 from pip._vendor.distlib.compat import raw_input
+from lib import text
 
 if __name__ == '__main__':
 
     name = raw_input("Name:")
+    name = name.lower()
     lastname = raw_input("LastName:")
-    bdate = raw_input("Birthday:")
+    lastname = lastname.lower()
+    year = raw_input("Year:")
     others = raw_input("More words, separate by , :")
+    others = others.lower()
     
-    print(name, lastname, bdate, others)
+        
+    #text.basicNameLast(name, lastname)    
+    #text.year(name, lastname, year)
+    #text.others(others)
+    #text.template()
+    text.permutations(name)
+    print("File done")
+
+
+    
+    
